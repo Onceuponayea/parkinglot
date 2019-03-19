d55c38362b31cbe78b54d68712c5b4b05fcffd33
xys3039 <303818615@qq.com>
2019-03-19 23:08:03 +0800

    测试查找vip的方法

--- src/com/smart/util/Test.java
+++ src/com/smart/util/Test.java
@@ -8,17 +8,37 @@ import java.util.TimerTask;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.smart.bean.CarBean;
+import com.smart.bean.VIPBean;
+import com.smart.dao.VipDaoInterface;
+import com.smart.dao.parkLotInterface;
+
 public class Test implements ServletContextListener{
 //	private Timer timer = null;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
-			long time = TimeUtil.sdfForSecond.parse("2019-03-21 00:00:01").getTime();
-			System.out.println(time/1000%86400-57600);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		DefaultSqlSessionFactory bean = (DefaultSqlSessionFactory) context.getBean("sqlSessionFactory");
+		SqlSession session = bean.openSession();
+		VipDaoInterface vipDaoInterface = session.getMapper(VipDaoInterface.class);
+		VIPBean vipBean = new VIPBean();
+		vipBean.setCarBean(new CarBean());
+		vipBean.getCarBean().setCarNum("闽D88888");
+		vipBean.setState("已生效");
+		VIPBean vipBean1 = vipDaoInterface.findVipByCarAndState(vipBean);
+		System.out.println(vipBean1.getCarBean().getCarId());
+//		try {
+//			long time = TimeUtil.sdfForSecond.parse("2019-03-21 00:00:01").getTime();
+//			System.out.println(time/1000%86400-57600);
+//		} catch (ParseException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 	}
 
